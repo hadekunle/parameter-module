8652226aa8b53a0c7e9c9a940f4f38812d898057
hadekunle <shitoheeb@gmail.com>
2025-07-22 19:47:53 -0500

    using window functions with lag

--- solutions/6.sql
+++ solutions/6.sql
@@ -1,3 +1,12 @@
 -- 6.⁠ ⁠Find the month-over-month percentage change 
 -- in total housing units completed.
 -- Use table: tutorial.us_housing_units
+
+
+select
+  year || ' ' || month_name as dt,
+  (south -lag(south) over (order by year))/lag(south) over (order by year) *100 as south_mtm_pct,
+  (west -lag(west) over (order by year))/lag(west) over (order by year) *100 as west_mtm_pct,
+  (midwest -lag(midwest) over (order by year))/lag(midwest) over (order by year) *100 as midwest_mtm_pct,
+  (northeast -lag(northeast) over (order by year))/lag(northeast) over (order by year) *100 as northeast_mtm_pct
+from tutorial.us_housing_units t 
