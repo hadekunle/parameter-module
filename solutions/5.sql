4881d3ad817cdf20198b44cc833d27586edf26ed
hadekunle <shitoheeb@gmail.com>
2026-02-15 21:00:03 -0600

    clean up

--- solutions/5.sql
+++
@@ -1,4 +0,0 @@
--- 5.⁠ ⁠Create a report that combines housing units with a lookup table
--- of region names and codes.
--- Use tables: tutorial.us_housing_units + region_lookup (assumed mapping table)
-
